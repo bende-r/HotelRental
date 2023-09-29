45e7166a6a00b75cd49c6fe3708e06810da0435d
bende-r <br242424@mail.ru>
2023-09-29 18:33:41 +0300

    Добавьте файлы проекта.

---
+++ HotelRental/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelRental;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ HotelRental/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelRental;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
